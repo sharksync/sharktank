ffa94eaec000339839889026e6dedf72d139687c
Neil Bostrom <github@silvergames.net>
2017-05-09 22:53:42 +0100

    Added Sync V3 using Scale DB

---
+++ SharkTank3.0/AWSServerless/AWSServerless/Scale/Tables/Change.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWSServerless.Scale.Tables
+{
+    public class Change
+    {
+        [JsonProperty("change_id")]
+        public Guid Id { get; set; }
+        [JsonProperty("rec_id")]
+        public Guid RecordId { get; set; }
+        [JsonProperty("path")]
+        public string Path { get; set; }
+        [JsonProperty("device_id")]
+        public Guid DeviceId { get; set; }
+        [JsonProperty("tidemark")]
+        public string Tidemark { get; set; }
+        [JsonProperty("modified")]
+        public DateTime Modified { get; set; }
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
