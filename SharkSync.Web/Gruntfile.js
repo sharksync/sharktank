3afb4b99973aed741d49387ca73617966e93a143
Neil Bostrom <github@silvergames.net>
2018-04-30 20:28:41 +0100

    Added cache busting to build

---
+++ SharkSync.Web/Gruntfile.js
@@ -0,0 +1,25 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        cacheBust: {
+            indexCacheBust: {
+                options: {
+                    deleteOriginals: true,
+                    assets: ['dist/*.js', 'dist/*.css'],
+                    baseDir: 'wwwroot/'
+                },
+                src: ['wwwroot/index.html']
+            }
+        }
+
+    });
+
+    // Plugins used
+    grunt.loadNpmTasks('grunt-cache-bust');
+
+    // Register pre and post build tasks
+    grunt.registerTask('postBuild', ['cacheBust:indexCacheBust']);
+
+};
