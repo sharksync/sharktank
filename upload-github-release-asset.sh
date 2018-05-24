785ddc4bc7cb1eda7dfe8008d7bc34840ba1f357
Neil Bostrom <github@silvergames.net>
2018-05-24 22:57:15 +0100

    Added version number detection to github release create script

--- upload-github-release-asset.sh
+++ upload-github-release-asset.sh
@@ -1,24 +1,4 @@
 #!/usr/bin/env bash
-#
-# Author: Stefan Buck
-# License: MIT
-# https://gist.github.com/stefanbuck/ce788fee19ab6eb0b4447a85fc99f447
-#
-#
-# This script accepts the following parameters:
-#
-# * owner
-# * repo
-# * tag
-# * filename
-# * github_api_token
-#
-# Script to upload a release asset using the GitHub API v3.
-#
-# Example:
-#
-# upload-github-release-asset.sh github_api_token=TOKEN owner=stefanbuck repo=playground tag=v0.1.0 filename=./build.zip
-#
 
 # Check dependencies.
 set -e
@@ -36,24 +16,37 @@ done
 # Define variables.
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/$owner/$repo"
-GH_TAGS="$GH_REPO/releases/tags/$tag"
+GH_RELEASES="$GH_REPO/releases/latest"
 AUTH="Authorization: token $github_api_token"
 WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
 
-if [[ "$tag" == 'LATEST' ]]; then
-  GH_TAGS="$GH_REPO/releases/latest"
-fi
-
 # Validate token.
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
-# Read asset tags.
-response=$(curl -sH "$AUTH" $GH_TAGS)
+# Read latest release
+releasesResponse=$(curl -sH "$AUTH" $GH_RELEASES)
 
-# Get ID of the asset based on given filename.
-eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
+# Get version of the last release
+echo "$releasesResponse" | grep -m 1 "name.:"
+eval $(echo "$releasesResponse" | grep -m 1 "name.:" | grep -w name | tr : = | tr -cd '[[:alnum:]]=')
+#[ "$version" ] || { echo "Error: Failed to get release version for latest release"; echo "$releasesResponse" | awk 'length($0)<100' >&2; exit 1; }
+
+echo "Found version: $version"
+
+$nextVersion = (echo $version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+
+echo "Moved to version: $nextVersion"
+
+# Create a new release
+curl -sH "$AUTH" --data '{"tag_name":"$nextVersion","target_commitish":"master","name":"$nextVersion","body":"Release of version $nextVersion","draft":false,"prerelease":false}' $GH_RELEASES
+
+# Read latest release
+releasesResponse=$(curl -sH "$AUTH" $GH_RELEASES)
+
+# Get ID of the release
+eval $(echo "$releasesResponse" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for latest release"; echo "$releasesResponse" | awk 'length($0)<100' >&2; exit 1; }
 
 # Upload asset
 echo "Uploading asset... "
@@ -61,4 +54,4 @@ echo "Uploading asset... "
 # Construct url
 GH_ASSET="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $filename)"
 
-curl "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "Authorization: token $github_api_token" -H "Content-Type: application/octet-stream" $GH_ASSET
+curl "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_ASSET
