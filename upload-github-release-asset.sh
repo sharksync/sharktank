96e5790f775a2e8edd978a04ff7ab2c0caad0645
Neil Bostrom <github@silvergames.net>
2018-05-24 22:58:11 +0100

    Added actual fixed file

--- upload-github-release-asset.sh
+++ upload-github-release-asset.sh
@@ -16,7 +16,7 @@ done
 # Define variables.
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/$owner/$repo"
-GH_RELEASES="$GH_REPO/releases/latest"
+GH_RELEASES="$GH_REPO/releases"
 AUTH="Authorization: token $github_api_token"
 WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
@@ -25,24 +25,22 @@ CURL_ARGS="-LJO#"
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
 # Read latest release
-releasesResponse=$(curl -sH "$AUTH" $GH_RELEASES)
+releasesResponse=$(curl -sH "$AUTH" "$GH_RELEASES/latest")
 
 # Get version of the last release
-echo "$releasesResponse" | grep -m 1 "name.:"
-eval $(echo "$releasesResponse" | grep -m 1 "name.:" | grep -w name | tr : = | tr -cd '[[:alnum:]]=')
-#[ "$version" ] || { echo "Error: Failed to get release version for latest release"; echo "$releasesResponse" | awk 'length($0)<100' >&2; exit 1; }
+version=$(echo "$releasesResponse" | grep -oP '(?<="tag_name": ")[^"]*')
 
 echo "Found version: $version"
 
-$nextVersion = (echo $version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+nextVersion=$(echo $version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
 
-echo "Moved to version: $nextVersion"
+echo "Next version: $nextVersion"
 
 # Create a new release
-curl -sH "$AUTH" --data '{"tag_name":"$nextVersion","target_commitish":"master","name":"$nextVersion","body":"Release of version $nextVersion","draft":false,"prerelease":false}' $GH_RELEASES
+curl -sH "$AUTH" --data "{\"tag_name\":\"$nextVersion\",\"target_commitish\":\"master\",\"name\":\"$nextVersion\",\"body\":\"Release of version $nextVersion\",\"draft\":false,\"prerelease\":false}" $GH_RELEASES
 
 # Read latest release
-releasesResponse=$(curl -sH "$AUTH" $GH_RELEASES)
+releasesResponse=$(curl -sH "$AUTH" "$GH_RELEASES/latest")
 
 # Get ID of the release
 eval $(echo "$releasesResponse" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
