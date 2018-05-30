59692651486779e9c22e9b298bf27dfd2486a45c
Neil Bostrom <github@silvergames.net>
2018-05-30 18:58:53 +0100

    Updated nuget packages

--- SharkSync.Deployment/UnpackZipIntoS3BucketFunction.cs
+++ SharkSync.Deployment/UnpackZipIntoS3BucketFunction.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
