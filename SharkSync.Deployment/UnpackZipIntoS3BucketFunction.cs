6d87e318e8d7851f80eb5633e17dde6ef2fa63d4
Neil Bostrom <github@silvergames.net>
2018-06-11 18:20:51 +0100

    Updated bucket reference and added try catch to lambda unzip

--- SharkSync.Deployment/UnpackZipIntoS3BucketFunction.cs
+++ SharkSync.Deployment/UnpackZipIntoS3BucketFunction.cs
@@ -42,36 +42,44 @@ namespace SharkSync.Deployment
 
         public async Task<CloudFormationResponse> FunctionHandlerAsync(UnpackZipIntoS3BucketRequest request, ILambdaContext context)
         {
-            context.Logger.Log("UnpackZipIntoS3BucketFunction invoked: " + JsonConvert.SerializeObject(request));
-
-            if (request.RequestType != "Delete")
+            try
             {
-                using (var zipStream = await S3Client.GetObjectStreamAsync(request.ResourceProperties.ZipS3Bucket, request.ResourceProperties.ZipS3Key, null))
-                {
-                    using (var zipArchive = new ZipArchive(zipStream))
-                    {
-                        foreach (var entry in zipArchive.Entries)
-                        {
-                            using (var entryStream = entry.Open())
-                            {
-                                using (var ms = new MemoryStream())
-                                {
-                                    entryStream.CopyTo(ms);
 
-                                    await S3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
+                context.Logger.Log("UnpackZipIntoS3BucketFunction invoked: " + JsonConvert.SerializeObject(request));
+
+                if (request.RequestType != "Delete")
+                {
+                    using (var zipStream = await S3Client.GetObjectStreamAsync(request.ResourceProperties.ZipS3Bucket, request.ResourceProperties.ZipS3Key, null))
+                    {
+                        using (var zipArchive = new ZipArchive(zipStream))
+                        {
+                            foreach (var entry in zipArchive.Entries)
+                            {
+                                using (var entryStream = entry.Open())
+                                {
+                                    using (var ms = new MemoryStream())
                                     {
-                                        BucketName = request.ResourceProperties.OutputS3Bucket,
-                                        Key = Path.Combine(request.ResourceProperties.OutputPrefix ?? "", entry.FullName),
-                                        InputStream = ms
-                                    });
+                                        entryStream.CopyTo(ms);
+
+                                        await S3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
+                                        {
+                                            BucketName = request.ResourceProperties.OutputS3Bucket,
+                                            Key = Path.Combine(request.ResourceProperties.OutputPrefix ?? "", entry.FullName),
+                                            InputStream = ms
+                                        });
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
 
-            return await CloudFormationResponse.CompleteCloudFormationResponse(null, request, context);
+                return await CloudFormationResponse.CompleteCloudFormationResponse(null, request, context);
+            }
+            catch (Exception ex)
+            {
+                return await CloudFormationResponse.CompleteCloudFormationResponse(ex, request, context);
+            }
         }
     }
 }
