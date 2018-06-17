14b4f31148691596752eebeea43ac4d15ac5ddb4
Neil Bostrom <github@silvergames.net>
2018-06-17 21:23:52 +0100

    Added integration tests

---
+++ SharkSync.IntegrationTests/UIAutomationTests/Common/TestSetupTeardown.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System.Reflection;
+using System.Drawing;
+
+namespace SharkSync.IntegrationTests.UIAutomationTests
+{
+    [TestFixture("Chrome")]
+    //[TestFixture("Firefox")]
+    public partial class Tests
+    {
+        protected IWebDriver driver;
+
+        private string browser;
+
+        public Tests(string browser)
+        {
+            this.browser = browser;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            string unitTestPath = Path.GetDirectoryName(Assembly.GetAssembly(typeof(Tests)).Location);
+
+            if (browser == "Chrome")
+            {
+                driver = new ChromeDriver(unitTestPath);
+            }
+            else if (browser == "Firefox")
+            {
+                driver = new FirefoxDriver(unitTestPath);
+            }
+            else
+            {
+                throw new Exception("Unsupported browser driver for local");
+            }
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SharkSync.IntegrationTests/UIAutomationTests/LoginTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharkSync.IntegrationTests.UIAutomationTests
+{
+    public partial class Tests
+    {
+        [Test]
+        public void LoginTests_Github()
+        {
+            driver.Navigate().GoToUrl("https://www.testingallthethings.net");
+
+            driver.FindElement(By.CssSelector(".navbar-nav")).Click();
+            
+            driver.FindElement(By.CssSelector(".github-login")).Click();
+
+            Assert.True(driver.Url.Contains("https://github.com/login"));
+        }
+    }
+}
