6f3944bac03e41b9f63f1426a8539f03025d3af0
Neil Bostrom <github@silvergames.net>
2018-07-18 21:22:36 +0100

    Removed json properties on request

--- SharkSync.Web.Api/ViewModels/Sync/SyncRequestViewModel.cs
+++ SharkSync.Web.Api/ViewModels/Sync/SyncRequestViewModel.cs
@@ -8,48 +8,25 @@ namespace SharkSync.Web.Api.ViewModels
 {
     public class SyncRequestViewModel
     {
-        [JsonProperty("app_id")]
         public Guid AppId { get; set; }
-
-        [JsonProperty("app_api_access_key")]
         public Guid AppApiAccessKey { get; set; }
-
-        [JsonProperty("groups")]
         public List<GroupViewModel> Groups { get; set; }
-
-        [JsonProperty("changes")]
         public List<ChangeViewModel> Changes { get; set; }
-
 
         public class GroupViewModel
         {
-            [JsonProperty("group")]
             public string Group { get; set; }
-
-            [JsonProperty("tidemark")]
             public long? Tidemark { get; set; }
         }
 
         public class ChangeViewModel
         {
-            [JsonProperty("record_id")]
             public Guid RecordId { get; set; }
-
-            [JsonProperty("entity")]
             public string Entity { get; set; }
-
-            [JsonProperty("group")]
             public string Group { get; set; }
-
-            [JsonProperty("value")]
             public string Value { get; set; }
-
-            [JsonProperty("millisecondsAgo")]
             public double MillisecondsAgo { get; set; }
-
-            [JsonProperty("property")]
             public string Property { get; set; }
-
         }
     }
 }
